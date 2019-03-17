aed4bfc083c0530f74f4718d12a95c78af77691e
anvolkravt <anvolkravt@ya.ru>
2019-03-18 00:45:10 +0300

    feat: add function that check if given brackets sequence is correct

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (i = 0; i < str.length; i++) {
+      for (j = 0; j < bracketsConfig.length; j++) {
+        if (str[i] == bracketsConfig[j][0] && str[i+1] == bracketsConfig[j][1]) {
+        str = str.slice(0, i) + str.slice(i + 2);
+        i -= 2;
+        }
+      }
+    }
+  return str == "";
 }
